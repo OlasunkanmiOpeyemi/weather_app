e32e60bd0d4d80bace4f9f0d77c028ef869f9595
Olasunkanmiopeyemi <olasunkanmiopeyemi1021@gmail.com>
2023-04-22 11:10:33 +0100

    weatherapp

--- weather.js
+++ weather.js
@@ -14,17 +14,55 @@ formEl.addEventListener("submit", (event) => {
 
 async function getWeatherData(cityValue) {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5weather?q=${cityValue}&appid=${apikey}&units=metric`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&APPID=${apikey}&units=metric`);
 
     if (!response.ok) {
-      throw new Error("Network response was not ok")
+      throw new Error("Network response was not ok");
     }
+    const data = await response.json();
 
-    const data = await response.json()
+    const temperature = Math.round(data.main.temp)
 
-    console.log(data);
+    const description = data.weather[0].description
+
+    const icon = data.weather[0].icon
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}°C`, `Humidity: ${data.main.humidity}%`,
+      `Wind speed: ${data.wind.speed} m/s`
+    ]
+
+    weatherDataEl.querySelector(".icon").innerHTML = ` <img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather icon">`;
+    weatherDataEl.querySelector(".temperature").textContent = `${temperature}°C`
+    weatherDataEl.querySelector(".description").textContent = description;
+    weatherDataEl.querySelector(".details").innerHTML = details.map((detail) => `<div>${detail}</div>`).join("");
+     
   } catch (error) {
-    
+      weatherDataEl.querySelector(".icon").innerHTML = "";
+    weatherDataEl.querySelector(".temperature").textContent = "";
+    weatherDataEl.querySelector(".description").textContent = "An error occured, please try again later!";
+
+    weatherDataEl.querySelector(".details").innerHTML = "";
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
